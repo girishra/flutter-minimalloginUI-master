1015b60ca78beee5c4b1bdae58615b1db1c77c7b
Lincoln Kushwah <lincolnkushwah009@gmail.com>
2019-09-27 15:37:43 +0530

    Initial commit

---
+++ lib/components/dashboard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class dashboard extends StatefulWidget {
+  @override
+  _dashboardState createState() => _dashboardState();
+}
+
+class _dashboardState extends State<dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+
+  }
+}
+
